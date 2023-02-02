6eca949bae14f81fc68b9ae174a25052435f9b50
wiz64 <67432394+wiz64@users.noreply.github.com>
2023-02-02 19:22:11 +0000

    patch downloader test

--- src/get_patches.cpp
+++ src/get_patches.cpp
@@ -3,8 +3,25 @@
 #include "defs.h"
 #include <curl/curl.h>
 
-
-
-extern "C" void get_patches(){
-        printf("\n Downloading patches...\n");
+// download file from url to file
+// return 0 on success
+extern "C" void get_patches()
+{
+        const char *url = "https://files.testfile.org/ZIPC/15MB-Corrupt-Testfile.Org.zip";
+        const char *file = "patches/test_patches_file.zip";
+        printf("Downloading %s to %s \n", url, file);
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+    curl = curl_easy_init();
+    if (curl)
+    {
+        fp = fopen(file, "wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        fclose(fp);
+    }
 }
