a001fdcd3e8d3cc29abee93bb68943aaa2394e3b
Soham Nandy <soham.nandy@outlook.com>
2023-02-02 11:42:26 +0530

    Added basic option parsing!

--- src/main.c
+++ src/main.c
@@ -25,8 +25,51 @@ static bool _verbose;
 static const char *custom_config_path = NULL;
 static const char *custom_output_path = NULL;
 static const char *custom_kernel_path = NULL;
-
 int main(int argc, char **argv)
 {
-        PANIC("Still WIP");
+        int c;
+        // get options
+        while (1)
+        {
+                static struct option long_options[] =
+                    {
+                        {"config", required_argument, 0, 'c'},
+                        {"output", required_argument, 0, 'o'},
+                        {0, 0, 0, 0}};
+                /* getopt_long stores the option index here. */
+                int option_index = 0;
+
+                c = getopt_long(argc, argv, "c:o:",
+                                long_options, &option_index);
+
+                /* Detect the end of the options. */
+                if (c == -1)
+                        break;
+
+                switch (c)
+                {
+                case 0:
+                        /* If this option set a flag, do nothing else now. */
+                        if (long_options[option_index].flag != 0)
+                                break;
+                        printf("option %s", long_options[option_index].name);
+                        if (optarg)
+                                printf(" with arg %s", optarg);
+                        printf("\n");
+                        break;
+
+                case 'c':
+                        custom_config_path = optarg;
+                        break;
+
+                case 'o':
+                        custom_output_path = optarg;
+                        break;
+                case '?':
+                        /* getopt_long already printed an error message. */
+                        break;
+                }
+        }
+
+
 }
